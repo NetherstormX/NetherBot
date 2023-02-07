a236fa4193a66dec7a9694b1c9042acaf1ebc44a
NetherstormX <94380113+NetherstormX@users.noreply.github.com>
2023-02-07 23:22:36 +0100

    Update netherbot.lua

--- netherbot.lua
+++ netherbot.lua
@@ -1,5 +1,5 @@
--- Create the frame
-local frame = CreateFrame("Frame", "MyAddonFrame", UIParent)
+-- Create the Main frame
+local frame = CreateFrame("Frame", "NetherbotFrame", UIParent)
 frame:SetSize(200, 200)
 frame:SetPoint("CENTER", UIParent, "CENTER")
 
@@ -23,14 +23,14 @@ frame:EnableMouse(true)
 
 
 -- Create the adminFrame
-local adminFrame = CreateFrame("Frame", "MyAddonAdminFrame", UIParent)
+local adminFrame = CreateFrame("Frame", "NetherbotAdminFrame", UIParent)
 adminFrame:SetSize(200, 200)
 adminFrame:SetPoint("RIGHT", frame, "LEFT", -10, 0)
 adminFrame:SetBackdrop({bgFile = "Interface/Tooltips/UI-Tooltip-Background", 
                       edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
                       tile = true, tileSize = 16, edgeSize = 16, 
                       insets = { left = 4, right = 4, top = 4, bottom = 4 }})
-adminFrame:SetBackdropColor(1, 0, 0, 0.2) -- Set the background color to red and transparency to 20%
+adminFrame:SetBackdropColor(1, 0, 0, 0.2) -- Set the background color to red and transparency to 20%.
 adminFrame:SetBackdropBorderColor(0, 1, 0,1)
 adminFrame:Hide() -- hide the admin frame by default
 
@@ -39,14 +39,14 @@ adminTitle:SetPoint("TOP", adminFrame, "TOP", 0, -10)
 adminTitle:SetText("Admin")
 
 
--- handle frame movement
+-- Handle frame movement
     frame:SetScript("OnMouseDown", function(self, button)
         if button == "LeftButton" then
           self:StartMoving()
         end
       end)
 
--- stop frame movement
+-- Stop frame movement
 frame:SetScript("OnMouseUp", function(self, button)
   if button == "LeftButton" then
     self:StopMovingOrSizing()
@@ -56,7 +56,7 @@ end)
 
 -- Create the buttons
 -- Follow Button
-local followButton = CreateFrame("Button", "MyAddonFollowButton", frame, "ActionButtonTemplate")
+local followButton = CreateFrame("Button", "NetherbotFollowButton", frame, "ActionButtonTemplate")
 followButton:SetPoint("TOPLEFT", frame, "TOPLEFT", 30, -35)
 followButton:SetSize(45, 45)
 followButton:SetText("Follow")
@@ -71,7 +71,7 @@ followpushedTexture:SetAllPoints()
 followButton:SetPushedTexture(followpushedTexture)
 
 -- StandStill Button
-local standstillButton = CreateFrame("Button", "MyAddonStandstillButton", frame, "ActionButtonTemplate")
+local standstillButton = CreateFrame("Button", "NetherbotStandstillButton", frame, "ActionButtonTemplate")
 standstillButton:SetPoint("LEFT", followButton, "RIGHT", 5, 0)
 standstillButton:SetSize(45, 45)
 standstillButton:SetText("Stand")
@@ -86,7 +86,7 @@ standstillpushedTexture:SetAllPoints()
 standstillButton:SetPushedTexture(standstillpushedTexture)
 
 --FullStop Button
-local fullstopButton = CreateFrame("Button", "MyAddonfullstopButton", frame, "ActionButtonTemplate")
+local fullstopButton = CreateFrame("Button", "NetherbotfullstopButton", frame, "ActionButtonTemplate")
 fullstopButton:SetPoint("LEFT", standstillButton, "RIGHT", 5, 0)
 fullstopButton:SetSize(45, 45)
 fullstopButton:SetText("Stop")
@@ -105,7 +105,7 @@ distanceLabel:SetPoint("BOTTOM", standstillButton, "BOTTOM", 0, -15)
 distanceLabel:SetText("Follow Distance:")
 
 --Distance1 Button
-local distance1Button = CreateFrame("Button", "MyAddondistance1Button", frame, "ActionButtonTemplate")
+local distance1Button = CreateFrame("Button", "Netherbotdistance1Button", frame, "ActionButtonTemplate")
 distance1Button:SetPoint("TOPLEFT", followButton, "BOTTOMLEFT", -5, -20)
 distance1Button:SetSize(50, 25)
 distance1Button:SetText("Low")
@@ -120,7 +120,7 @@ distance1pushedTexture:SetAllPoints()
 distance1Button:SetPushedTexture(distance1pushedTexture)
 
 --Distance2 Button
-local distance2Button = CreateFrame("Button", "MyAddondistance2Button", frame, "ActionButtonTemplate")
+local distance2Button = CreateFrame("Button", "Netherbotdistance2Button", frame, "ActionButtonTemplate")
 distance2Button:SetPoint("LEFT", distance1Button, "RIGHT", 5, 0)
 distance2Button:SetSize(50, 25)
 distance2Button:SetText("Medium")
@@ -135,7 +135,7 @@ distance2pushedTexture:SetAllPoints()
 distance2Button:SetPushedTexture(distance2pushedTexture)
 
 --Distance3 Button
-local distance3Button = CreateFrame("Button", "MyAddondistance3Button", frame, "ActionButtonTemplate")
+local distance3Button = CreateFrame("Button", "Netherbotdistance3Button", frame, "ActionButtonTemplate")
 distance3Button:SetPoint("LEFT", distance2Button, "RIGHT", 5, 0)
 distance3Button:SetSize(50, 25)
 distance3Button:SetText("High")
@@ -152,50 +152,50 @@ distance3Button:SetPushedTexture(distance3pushedTexture)
 
 
 
-local adminButton = CreateFrame("Button", "MyAddonAdminButton", frame, "UIPanelButtonTemplate")
+local adminButton = CreateFrame("Button", "NetherbotAdminButton", frame, "UIPanelButtonTemplate")
 adminButton:SetSize(60, 20)
 adminButton:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -10, 10) -- position the button in the bottom right of the frame
 adminButton:SetText("Admin")
 adminButton:GetNormalTexture():SetVertexColor(0.10,1.00,0.10)
 
 -- Create Admin Buttons
-local buttonAdd = CreateFrame("Button", "MyAddonButtonAdd", adminFrame, "UIPanelButtonTemplate")
+local buttonAdd = CreateFrame("Button", "NetherbotButtonAdd", adminFrame, "UIPanelButtonTemplate")
 buttonAdd:SetSize(56, 22)
 buttonAdd:SetPoint("TOPLEFT", adminFrame, "TOPLEFT", 10, -35)
 buttonAdd:SetText("Add")
 buttonAdd:GetNormalTexture():SetVertexColor(0.10,1.00,0.10)
 
-local buttonRemove = CreateFrame("Button", "MyAddonButtonRemove", adminFrame, "UIPanelButtonTemplate")
+local buttonRemove = CreateFrame("Button", "NetherbotButtonRemove", adminFrame, "UIPanelButtonTemplate")
 buttonRemove:SetSize(65, 22)
 buttonRemove:SetPoint("TOP", buttonAdd, "BOTTOM", 25, -5)
 buttonRemove:SetText("Remove")
 buttonRemove:GetNormalTexture():SetVertexColor(0.10,1.00,0.10)
 
-local buttonRecall = CreateFrame("Button", "MyAddonButtonRecall", adminFrame, "UIPanelButtonTemplate")
+local buttonRecall = CreateFrame("Button", "NetherbotButtonRecall", adminFrame, "UIPanelButtonTemplate")
 buttonRecall:SetSize(59, 22)
 buttonRecall:SetPoint("LEFT", buttonAdd, "RIGHT", 3, 0)
 buttonRecall:SetText("Recall")
 buttonRecall:GetNormalTexture():SetVertexColor(0.10,1.00,0.10)
 
-local buttonBotinfo = CreateFrame("Button", "MyAddonButtonBotInfo", adminFrame, "UIPanelButtonTemplate")
+local buttonBotinfo = CreateFrame("Button", "NetherbotButtonBotInfo", adminFrame, "UIPanelButtonTemplate")
 buttonBotinfo:SetSize(62, 22)
 buttonBotinfo:SetPoint("LEFT", buttonRecall, "RIGHT", 2, 0)
 buttonBotinfo:SetText("Bot-Info")
 buttonBotinfo:GetNormalTexture():SetVertexColor(0.10,1.00,0.10)
 
-local buttonMove = CreateFrame("Button", "MyAddonButtonMove", adminFrame, "UIPanelButtonTemplate")
+local buttonMove = CreateFrame("Button", "NetherbotButtonMove", adminFrame, "UIPanelButtonTemplate")
 buttonMove:SetSize(65, 22)
 buttonMove:SetPoint("LEFT", buttonRemove, "RIGHT", 5, 0)
 buttonMove:SetText("Move")
 buttonMove:GetNormalTexture():SetVertexColor(0.10,1.00,0.10)
 
-local buttonDelete = CreateFrame("Button", "MyAddonButtonDelete", adminFrame, "UIPanelButtonTemplate")
+local buttonDelete = CreateFrame("Button", "NetherbotButtonDelete", adminFrame, "UIPanelButtonTemplate")
 buttonDelete:SetSize(60, 22)
 buttonDelete:SetPoint("BOTTOMRIGHT", adminFrame, "BOTTOMRIGHT", -10, 10)
 buttonDelete:SetText("Delete")
 buttonDelete:GetNormalTexture():SetVertexColor(0.10,1.00,0.10)
 
-local redemptionButton = CreateFrame("Button", "MyAddonRedemptionButton", adminFrame, "SecureActionButtonTemplate")
+local redemptionButton = CreateFrame("Button", "NetherbotRedemptionButton", adminFrame, "SecureActionButtonTemplate")
 redemptionButton:SetSize(30, 30)
 redemptionButton:SetPoint("BOTTOMLEFT", adminFrame, "BOTTOMLEFT", 10, 10)
 
@@ -209,7 +209,7 @@ redemptionButton:SetAttribute("type", "spell")
 redemptionButton:SetAttribute("spell", 7328)
 
 -- Create the "botLookupButton" button
-local botLookupButton = CreateFrame("Button", "MyAddonBotLookupButton", adminFrame, "UIPanelButtonTemplate")
+local botLookupButton = CreateFrame("Button", "NetherbotBotLookupButton", adminFrame, "UIPanelButtonTemplate")
 botLookupButton:SetSize(65, 22)
 botLookupButton:SetPoint("BOTTOM", adminFrame, "BOTTOM", 0, 10)
 botLookupButton:SetText("Lookup")
@@ -309,6 +309,11 @@ buttonAdd:SetScript("OnClick", function()
     SendChatMessage(".npcbot recall", "SAY")
   end)
 
+  buttonBotinfo:SetScript("OnClick", function()
+    SendChatMessage(".npcbot info", "SAY")
+    DoEmote("BONK")
+  end)
+
   buttonMove:SetScript("OnClick", function()
     SendChatMessage(".npcbot move", "SAY")
   end)
@@ -367,7 +372,7 @@ end)
   end)
 
 -- Create the lookup frame
-local lookupFrame = CreateFrame("Frame", "MyAddonLookupFrame", UIParent)
+local lookupFrame = CreateFrame("Frame", "NetherbotLookupFrame", UIParent)
 lookupFrame:SetSize(200, 200)
 lookupFrame:SetPoint("CENTER", UIParent, "CENTER")
 lookupFrame:SetBackdrop({
@@ -386,12 +391,12 @@ lookupTitle:SetText("Select class:")
 
 
 -- Create the scrollframe for the list
-local lookupScrollFrame = CreateFrame("ScrollFrame", "MyAddonLookupScrollFrame", lookupFrame, "UIPanelScrollFrameTemplate")
+local lookupScrollFrame = CreateFrame("ScrollFrame", "NetherbotLookupScrollFrame", lookupFrame, "UIPanelScrollFrameTemplate")
 lookupScrollFrame:SetPoint("TOPLEFT", lookupFrame, "TOPLEFT", 4, -25)
 lookupScrollFrame:SetPoint("BOTTOMRIGHT", lookupFrame, "BOTTOMRIGHT", -4, 4)
 
 -- Create the list frame
-local lookupList = CreateFrame("Frame", "MyAddonLookupList", lookupScrollFrame)
+local lookupList = CreateFrame("Frame", "NetherbotLookupList", lookupScrollFrame)
 lookupList:SetSize(lookupScrollFrame:GetWidth(), lookupScrollFrame:GetHeight())
 lookupScrollFrame:SetScrollChild(lookupList)
 
@@ -419,7 +424,7 @@ local classTable = {
 
 -- Create the buttons for the list items
 for key, value in pairs(classTable) do
-  local button = CreateFrame("Button", "MyAddonLookupButton"..value, lookupList, "UIPanelButtonTemplate")
+  local button = CreateFrame("Button", "NetherbotLookupButton"..value, lookupList, "UIPanelButtonTemplate")
   button:SetSize(180, 25)
   button:SetPoint("TOPLEFT", lookupList, "TOPLEFT", 10, -10 - (value-1)*30)
   button:SetText(key)
@@ -433,7 +438,7 @@ for key, value in pairs(classTable) do
 end
 
 -- Create the "hideLookup" button
-local hideLookupButton = CreateFrame("Button", "MyAddonHideLookupButton", lookupFrame, "UIPanelButtonTemplate")
+local hideLookupButton = CreateFrame("Button", "NetherbotHideLookupButton", lookupFrame, "UIPanelButtonTemplate")
 hideLookupButton:SetSize(21, 20)
 hideLookupButton:SetPoint("TOPRIGHT", lookupFrame, "TOPRIGHT", -10, -8)
 hideLookupButton:SetText("X")
@@ -441,7 +446,7 @@ hideLookupButton:GetNormalTexture():SetVertexColor(0.10,1.00,0.10)
 
 
 -- Create the spawnFrame
-local spawnFrame = CreateFrame("Frame", "MyAddonSpawnFrame", lookupFrame)
+local spawnFrame = CreateFrame("Frame", "NetherbotSpawnFrame", lookupFrame)
 spawnFrame:SetSize(200, 60)
 spawnFrame:SetPoint("BOTTOM", lookupFrame, "BOTTOM", 0, -70)
 spawnFrame:SetBackdrop({
@@ -459,34 +464,35 @@ spawnTitle:SetText("Spawn BOT ID:")
 
 
 -- Create the "buttonSpawnBot" button
-local buttonSpawnBot = CreateFrame("Button", "MyAddonButtonSpawnBot", spawnFrame, "UIPanelButtonTemplate")
+local buttonSpawnBot = CreateFrame("Button", "NetherbotButtonSpawnBot", spawnFrame, "UIPanelButtonTemplate")
 buttonSpawnBot:SetSize(80, 25)
 buttonSpawnBot:SetPoint("BOTTOMLEFT", spawnFrame, "BOTTOMLEFT", 10, 10)
 buttonSpawnBot:SetText("Spawn Bot")
 buttonSpawnBot:GetNormalTexture():SetVertexColor(0.10,1.00,0.10)
 
 -- Create the "classInput" input box
-local classInput = CreateFrame("EditBox", "MyAddonClassInput", spawnFrame, "InputBoxTemplate")
+local classInput = CreateFrame("EditBox", "NetherbotClassInput", spawnFrame, "InputBoxTemplate")
 classInput:SetSize(80, 25)
 classInput:SetPoint("BOTTOMLEFT", buttonSpawnBot, "BOTTOMRIGHT", 10, 0)
 classInput:SetAutoFocus(false)
 
--- Handle the button's click event
+-- Handle the buttons click event
 buttonSpawnBot:SetScript("OnClick", function()
   local input = classInput:GetText()
   if input ~= "" then
     SendChatMessage(".npcbot spawn "..input, "GUILD")
+    classInput:SetText("")
   else
-    print("Please enter a ID:")
+    print("Please enter an ID:")
   end
 end)
 
--- Handle the button's click event
+-- Handle Lookup buttons click event
 hideLookupButton:SetScript("OnClick", function()
     lookupFrame:Hide()
 end)
 
--- Handle the button's click event
+-- Handle the Lookup buttons click event
 botLookupButton:SetScript("OnClick", function()
   if lookupFrame:IsShown() then
     lookupFrame:Hide()
